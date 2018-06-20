b5bf112609d06f4100cea1f31f22873bd4547a5c
mano42023 <mano42023@gmail.com>
2018-06-20 17:43:15 +0530

    firt

---
+++ src/pages/home/home.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
+@Component({
+  selector: 'page-home',
+  providers: [BarcodeScanner],
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public barcode:string;
+  constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner) {
+
+  }
+
+
+
+ scan(){
+	this.barcodeScanner.scan().then((barcodeData) => {
+		this.barcode= barcodeData.text;  
+	}, (err) => {
+		this.barcode=err;
+	});
+ }
+}
